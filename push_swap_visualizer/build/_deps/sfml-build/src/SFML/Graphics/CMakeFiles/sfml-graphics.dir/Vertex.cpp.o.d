c2684c0e94f18b82bbec44c028b27f49cafb13d2
chikhidev <chikhi.dev@gmail.com>
2024-04-21 10:15:44 +0100

    tester

--- push_swap_visualizer/build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o: \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/Graphics/Vertex.cpp \
- /usr/include/stdc-predef.h \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Vertex.hpp \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Color.hpp \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Vector2.hpp \
- /home/abdoo/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Vector2.inl
